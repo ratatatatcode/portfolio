ebcf1e17863897433ed6822dff7346f84319ef78
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-05-04 20:32:58 +0800

    Clean up portfolio and restart with fresh Next.js setup

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mt-4 flex min-h-screen w-full flex-col items-center justify-start gap-10 px-4 py-10 antialiased md:px-[25%]">
-      {children}
-    </div>
-  );
+  return <div className="flex min-h-screen w-full flex-col antialiased">{children}</div>;
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,19 +1,7 @@
-import IntroductionSection from '../../components/main/introduction';
-import AboutSection from '../../components/main/about';
-import SkillsSection from '../../components/main/skills';
-import EducationSection from '../../components/main/education';
-import ExperienceSection from '@/components/main/experience';
-import ProjectsSection from '@/components/main/projects';
-
 export default function Home() {
   return (
     <>
-      <IntroductionSection />
-      <AboutSection />
-      <SkillsSection />
-      <EducationSection />
-      <ExperienceSection />
-      <ProjectsSection />
+      <h1>Hello World</h1>
     </>
   );
 }
