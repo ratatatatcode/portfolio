b7033d146a5dba56333deb3001672215941f7bf6
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-05-26 17:49:03 +0800

    Adjust global padding; Add neumorphism (introduction)

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center px-4 py-10 antialiased">
+    <div className="flex min-h-screen w-full flex-col items-center px-6 py-10 antialiased">
       {children}
     </div>
   );
--- src/components/landing/introduction.tsx
+++ src/components/landing/introduction.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image';
+
 export default function Introduction() {
   return (
-    <section className="flex h-auto flex-col md:w-[50%]">
+    <section className="relative flex h-auto flex-col rounded-md bg-[#e0e0e0] p-10 shadow-[20px_20px_60px_#bebebe,-20px_-20px_60px_#ffffff] md:w-[50%]">
       <h1 className="text-5xl">
         Hi, <span className="font-extrabold">I am James</span>
       </h1>
@@ -11,10 +13,17 @@ export default function Introduction() {
         <strong>Full Stack Developer</strong> and Computer Science <br></br>undergrad, skilled in
         JavaScript.
       </p>
-      <hr className="my-2 w-full" />
+      <hr className="border-muted-foreground/50 my-2 w-full" />
       <p className="text-xs font-semibold">
         <span>HTMLCSSJS NODEJS REACTJS NEXTJS PYTHON SQL/NOSQL</span>
       </p>
+      <Image
+        src="/logo/wave.png"
+        width={60}
+        height={60}
+        alt="Logo"
+        className="waving-character absolute top-10 right-5 drop-shadow-xl md:right-0 md:h-35 md:w-35 lg:right-5"
+      />
     </section>
   );
 }
