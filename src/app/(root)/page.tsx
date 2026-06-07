6f237f9a62f2d6595724ff040c5883f84d378238
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-06-07 20:24:43 +0800

    Move layout padding to sections

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <>
       <section className="hidden md:block md:w-[10%]"></section>
-      <section className="scrollbar-hide w-full gap-6 overflow-y-auto md:h-screen md:w-[60%] md:gap-4">
+      <section className="scrollbar-hide w-full gap-6 overflow-y-auto p-6 md:h-screen md:w-[60%] md:gap-4 md:p-4">
         <IntroductionComponent />
         <AboutComponent />
       </section>
-      <section className="scrollbar-hide flex h-screen w-full flex-col items-center gap-4 overflow-y-auto md:w-[30%]">
+      <section className="scrollbar-hide flex h-screen w-full flex-col items-center gap-4 overflow-y-auto p-6 md:w-[30%] md:p-4">
         <OtherProjectsCompoent />
         <ProjectListComponent />
       </section>
