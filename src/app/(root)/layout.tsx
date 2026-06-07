3a6bd11b05c0de2621bd580fb947cd87e79e519c
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-06-07 20:32:15 +0800

    Remove layout gap

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center gap-10 antialiased md:flex-row md:gap-0">
+    <div className="flex min-h-screen w-full flex-col items-center antialiased md:flex-row">
       {children}
     </div>
   );
