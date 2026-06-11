ad040379da5fed4b265e391baef46735957d9a05
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-06-11 19:31:23 +0800

    Add badge

---
+++ src/components/shared/badge.tsx
@@ -0,0 +1,24 @@
+export default function BadgeComponent({
+  icon,
+  text,
+  bg_color,
+  text_color,
+}: {
+  icon?: React.ReactNode;
+  text: string;
+  bg_color: string;
+  text_color: string;
+}) {
+  return (
+    <>
+      <p
+        className={`${bg_color} flex w-auto items-center gap-2 rounded-[5px] px-3 py-1 text-xs font-semibold ${
+          text_color == 'white' ? 'text-white' : 'text-black'
+        }`}
+      >
+        <span>{icon}</span>
+        {text}
+      </p>
+    </>
+  );
+}
