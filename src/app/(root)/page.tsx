f397f2bf8b10c4600b3bae9d38e2013afdd4452b
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-06-11 19:32:55 +0800

    Add skills list

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,6 +2,7 @@ import MenuComponent from '@/components/landing/modal/menu';
 import IntroductionComponent from '@/components/landing/introduction/introduction';
 import AboutComponent from '@/components/landing/about';
 import ExperienceComponent from '@/components/landing/experience';
+import SkillsListComponent from '@/components/landing/skills';
 import OtherProjectsCompoent from '@/components/landing/projects/otherProjects';
 import ProjectListComponent from '@/components/landing/projects/projects';
 
@@ -11,10 +12,11 @@ export default function Home() {
       <section className="hidden h-screen bg-gray-100 md:flex md:w-[5%] md:justify-center">
         <MenuComponent />
       </section>
-      <section className="scrollbar-hide flex w-full flex-col gap-6 overflow-y-auto p-6 md:h-screen md:w-[65%] md:gap-4 md:p-4">
+      <section className="scrollbar-hide flex w-full flex-col gap-6 overflow-y-auto p-6 md:h-screen md:w-[65%] md:gap-0 md:p-4">
         <IntroductionComponent />
         <AboutComponent />
         <ExperienceComponent />
+        <SkillsListComponent />
       </section>
       <section className="scrollbar-hide flex h-screen w-full flex-col items-center gap-4 overflow-y-auto p-6 md:w-[30%] md:p-4">
         <OtherProjectsCompoent />
---
+++ src/components/landing/skills.tsx
@@ -0,0 +1,40 @@
+import BadgeComponent from '../shared/badge';
+import { FaHtml5, FaJs, FaNodeJs, FaReact, FaFigma } from 'react-icons/fa';
+import { SiNextdotjs, SiExpress, SiMysql, SiFirebase, SiTailwindcss, SiCss } from 'react-icons/si';
+import { DiPhotoshop } from 'react-icons/di';
+
+export default function SkillsListComponent() {
+  const skills = [
+    { text: 'HTML5', icon: <FaHtml5 /> },
+    { text: 'JavaScript', icon: <FaJs /> },
+    { text: 'Node.js', icon: <FaNodeJs /> },
+    { text: 'Express.js', icon: <SiExpress /> },
+    { text: 'React.js', icon: <FaReact /> },
+    { text: 'Next.js', icon: <SiNextdotjs /> },
+    { text: 'MySQL', icon: <SiMysql /> },
+    { text: 'Firebase', icon: <SiFirebase /> },
+    { text: 'CSS3', icon: <SiCss /> },
+    { text: 'Tailwind CSS', icon: <SiTailwindcss /> },
+    { text: 'Figma', icon: <FaFigma /> },
+    { text: 'Photoshop', icon: <DiPhotoshop /> },
+  ];
+
+  return (
+    <div className="w-full text-justify text-sm md:w-[90%] md:p-4">
+      <h2 className="mb-2 text-2xl font-bold">
+        <u>SKILLS</u>
+      </h2>
+      <div className="flex w-full flex-wrap gap-1">
+        {skills.map((skill) => (
+          <BadgeComponent
+            key={skill.text}
+            text={skill.text}
+            icon={skill.icon}
+            bg_color="bg-black/80"
+            text_color="white"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
